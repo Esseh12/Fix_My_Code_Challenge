f1b3661d0c771e61d6f8a08f9b07f02ae256c379
Esseh12 <Gbenmoese788@gmailcom>
2024-05-15 18:42:04 +0100

    sqaure has equal side

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -13,13 +13,13 @@ class Square:
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def PerimiterOfMySquare(self):
         """perimeter of the square"""
-        return self.width * 4
+        return 4 * self.width
 
     def __str__(self):
         """documentation"""
-        return "{}".format(self.width)
+        return f"Square(width={self.width})"
 
 
 if __name__ == "__main__":
@@ -27,4 +27,4 @@ if __name__ == "__main__":
     s = Square(width=12)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.PerimiterOfMySquare())
